4157a533f1d4c64cef2c1e893ebab0b4d4b87a0b
christopi <moxi980617@outlook.com>
2025-01-20 11:57:47 -0500

    feat: add miner baseline, update config.py
    since the config system is updated rn, need to update the validator config

--- video_subnet_core/base/miner.py
+++ video_subnet_core/base/miner.py
@@ -10,21 +10,40 @@ class BaseMiner(ABC):
 
     def init_bittensor(self):
         self.subtensor = bt.subtensor(config=self.config)
+        logger.info(f"Subtensor: {self.subtensor}")
         self.wallet = bt.wallet(config=self.config)
+        logger.info(f"Wallet: {self.wallet}")
         self.metagraph = self.subtensor.metagraph(netuid=self.config.netuid)
+        logger.info(f"Metagraph: {self.metagraph}")
         self.axon = bt.axon(config=self.config)
         self.axon.attach(
             forward_fn=self.forward,
             blacklist_fn=self.blacklist,
+            priority_fn=self.priority,
         )
+        if self.wallet.hotkey.ss58_address not in self.metagraph.hotkeys:
+            logger.error(
+                f"\nYour Miner: {self.wallet} is not registered to the subnet{self.config.netuid} \nRun 'btcli register' and try again."
+            )
+            exit()
+        else:
+            self.my_subnet_uid = self.metagraph.hotkeys.index(
+                self.wallet.hotkey.ss58_address
+            )
+            logger.info(f"Running Miner on uid: {self.my_subnet_uid}")
         self.axon.serve(netuid=self.config.netuid, subtensor=self.subtensor)
         self.axon.start()
 
     def chain_sync(self):
-        self.metagraph.sync()
+        bt.logging.info("resync_metagraph()")
+        self.metagraph.sync(subtensor=self.subtensor)
 
     @abstractmethod
     async def forward(self, synapse: bt.Synapse) -> bt.Synapse: ...
 
     @abstractmethod
     async def blacklist(self, synapse: bt.Synapse) -> bool: ...
+
+    @abstractmethod
+    async def priority(self, synapse: bt.synapse) -> float: ...
+    
