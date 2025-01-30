4a7f186ba72da50eb8c67706547c12eb02441a08
christopi <moxi980617@outlook.com>
2025-01-30 14:25:14 +0000

    chore: update videoupscale protocol

--- video_subnet_core/configs/video_scheduler.py
+++ video_subnet_core/configs/video_scheduler.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field
 class VideoSchedulerConfig(BaseModel):
     host: str = Field(default="localhost")
     port: int = Field(default=8000)
-    max_synthetic_queue_size: int = Field(default=4)
-    refill_threshold: int = Field(default=2)
-    refill_target: int = Field(default=4)
+    max_synthetic_queue_size: int = Field(default=2)
+    refill_threshold: int = Field(default=1)
+    refill_target: int = Field(default=2)
     clip_duration: int = Field(default=1)
     min_video_len: int = Field(default=1)
     max_video_len: int = Field(default=5)
