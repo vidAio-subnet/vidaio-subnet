6b389c8d0394f181cd7df504467ad3c7028f76fc
christopi <moxi980617@outlook.com>
2025-02-03 05:33:54 +0000

    fix: fix bug during scoring endpoint configuration

--- video_subnet_core/configs/score.py
+++ video_subnet_core/configs/score.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class ScoreConfig(BaseModel):
     decay_factor: float = Field(default=0.9)
-    host: str = Field(default="http://localhost")
+    host: str = Field(default="localhost")
     port: int = Field(default=8200)
