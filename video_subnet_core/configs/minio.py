573bfe8b78550f4fbe70a594aa4edb532d190ac0
christopi <moxi980617@outlook.com>
2025-02-12 10:01:13 -0300

    chore: update minio configuration

--- video_subnet_core/configs/minio.py
+++ video_subnet_core/configs/minio.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 class MinioConfig(BaseModel):
-    endpoint: str = Field(default="localhost:9000")
-    access_key: str = Field(default="minioadmin")
-    secret_key: str = Field(default="minioadmin")
-    secure: bool = Field(default=False)
-    bucket_name: str = Field(default="video-subnet")
+    endpoint: str = Field(default=os.getenv("S3_COMPATIBLE_ENDPOINT", "localhost:9000"))
+    access_key: str = Field(default=os.getenv("S3_COMPATIBLE_ACCESS_KEY", "minioadmin"))
+    secret_key: str = Field(default=os.getenv("S3_COMPATIBLE_SECRET_KEY", "minioadmin"))
+    secure: bool = Field(default=True)
+    bucket_name: str = Field(default=os.getenv("BUCKET_NAME", "vidaio-subnet"))
