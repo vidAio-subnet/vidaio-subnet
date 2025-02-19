929dec738f8e14b756dd2bbd09db96df6a677d79
christopi <moxi980617@outlook.com>
2025-02-19 20:10:13 +0000

    chore: minio client

--- vidaio_subnet_core/configs/score.py
+++ vidaio_subnet_core/configs/score.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, Field
 class ScoreConfig(BaseModel):
     decay_factor: float = Field(default=0.9)
     host: str = Field(default="localhost")
-    port: int = Field(default=8200)
+    port: int = Field(default=8201)
