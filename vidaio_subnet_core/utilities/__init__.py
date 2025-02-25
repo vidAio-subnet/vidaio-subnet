47fab8ee6927a74220a08137ccab0c272761d640
christopi <moxi980617@outlook.com>
2025-02-25 14:32:19 +0000

    feat: add version param at UpscaleRequest also validator side

--- vidaio_subnet_core/utilities/__init__.py
+++ vidaio_subnet_core/utilities/__init__.py
@@ -1,7 +1,9 @@
 from .minio_client import minio_client
 from .file_handler import download_video
+from .version import version
 
 __all__ = [
     "minio_client",
     "download_video",
+    "version",
 ]
