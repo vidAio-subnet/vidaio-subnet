e53945fe83753895d2ef743a221edb58c2ae3c92
christopi <medfil777@gmail.com>
2025-04-18 04:15:51 +0000

    update request interval

--- vidaio_subnet_core/configs/bandwidth.py
+++ vidaio_subnet_core/configs/bandwidth.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class BandwidthConfig(BaseModel):
     total_requests: int = Field(default=64)
-    request_interval: int = Field(default=10000)
+    request_interval: int = Field(default=500)
     requests_per_interval: int = Field(default=20)
     # requests_organic_interval: int = Field(default = 10)
     requests_organic_interval: int = Field(default = 1)
