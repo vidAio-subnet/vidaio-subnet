a680cece46d257cc12965d0bad5182191be11807
christopi <medfil777@gmail.com>
2025-05-14 16:24:00 +0000

    chore: update synthetic requests miner interval per batch

--- vidaio_subnet_core/configs/bandwidth.py
+++ vidaio_subnet_core/configs/bandwidth.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel, Field
 class BandwidthConfig(BaseModel):
     total_requests: int = Field(default=64)
     request_interval: int = Field(default=500)
-    requests_per_synthetic_interval: int = Field(default=10)
+    requests_per_synthetic_interval: int = Field(default=5)
     requests_per_organic_interval: int = Field(default = 10)
     min_stake: int = Field(default=25000)
