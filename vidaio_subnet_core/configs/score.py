5875e43e2fda34fcba99526f53201f753c380141
christopi <medfil777@gmail.com>
2025-06-10 08:31:03 +0000

    chore: update moving average alpha

--- vidaio_subnet_core/configs/score.py
+++ vidaio_subnet_core/configs/score.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class ScoreConfig(BaseModel):
-    decay_factor: float = Field(default=0.95)
+    decay_factor: float = Field(default=0.9)
     host: str = Field(default="localhost")
     port: int = Field(default=8201)
     vmaf_threshold: float = Field(default=0.5)
