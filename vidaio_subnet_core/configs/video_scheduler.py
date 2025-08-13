fc1668db543403c197ecae9610c7a7890a6bc314
christopi <medfil777@gmail.com>
2025-08-13 14:11:56 +0000

    back video scheduler config

--- vidaio_subnet_core/configs/video_scheduler.py
+++ vidaio_subnet_core/configs/video_scheduler.py
@@ -5,12 +5,12 @@ class VideoSchedulerConfig(BaseModel):
     host: str = Field(default="localhost")
     port: int = Field(default=8000)
     max_synthetic_queue_size: int = Field(default=100) 
-    refill_threshold: int = Field(default=10) #  90 
-    refill_target: int = Field(default=10)  # 100
-    min_video_len: int = Field(default=30)# 30
-    max_video_len: int = Field(default=40)# 40
-    pexels_max_size: int = Field(default=5) # 110
-    pexels_threshold: int = Field(default=4)# 90
+    refill_threshold: int = Field(default=90) 
+    refill_target: int = Field(default=100)
+    min_video_len: int = Field(default=30)
+    max_video_len: int = Field(default=40)
+    pexels_max_size: int = Field(default=110)
+    pexels_threshold: int = Field(default=90)
     weight_hd_to_4k: float = Field(default=0.4)
     weight_sd_to_hd: float = Field(default=0.3)
     weight_sd_to_4k: float = Field(default=0.3)
