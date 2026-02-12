3e19c344127b1b11c233e89c7bdb01fd57f0f511
Arpan Tripathi <tripathiarpan20@gmail.com>
2026-02-12 07:18:57 +0000

    set burn to 33% from 50%

--- vidaio_subnet_core/validating/managing/miner_manager.py
+++ vidaio_subnet_core/validating/managing/miner_manager.py
@@ -28,7 +28,7 @@ class MinerManager:
 
         self.config = config
         self.subtensor = bt.subtensor(config=self.config)
-        self.burn_proportion = float(1/2)   # 1/2 of miner emissions burnt
+        self.burn_proportion = float(1/3)   # 1 of miner emissions burnt
 
         self.metagraph = metagraph
         self.config_url = CONFIG.sql.url
